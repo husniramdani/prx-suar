8e70ab01d8fecf3d565c4af0e50e9b04b5cb86bd
husniramdani <husniramdani18@gmail.com>
2023-09-23 17:05:02 +0700

    update config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@env': ['./src/utils/env/index'],
+        },
+      },
+    ],
+  ],
 };
